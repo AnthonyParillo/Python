d2925d1b842ac48f096906e7780dbb25e8e3e55a
Anthony Parillo <aparillo87@icloud.com>
2023-02-26 19:59:37 +0100

    Delinting Cleanup

--- Project_Encrypt/project_encrypt.py
+++ Project_Encrypt/project_encrypt.py
@@ -9,13 +9,12 @@ from pathlib import Path
 from tkinterdnd2 import *
 try:
     from Tkinter import *
-    from ScrolledText import ScrolledText
 except ImportError:
     from tkinter import *
-    from tkinter.scrolledtext import ScrolledText
 import tkinter as tk
 
 #################### Start Drop Box ####################
+
 
 def drop_box():
     root = TkinterDnD.Tk()
@@ -51,24 +50,25 @@ def drop_box():
 
     # Top Text / Instructions
     Label(root, text='Drag and Drop Encrypted File Here:').grid(
-                        row=0, column=0, padx=10, pady=5)
-    
+        row=0, column=0, padx=10, pady=5)
+
     # Destroy Funct. for Lambda
     def destroy():
         root.destroy()
 
-    # Quit Funct. for Lambda    
+    # Quit Funct. for Lambda
     def quit():
         root.quit()
 
     # If After File Add
     def file_added():
         Label(root, text='Is this the right file?:').grid(
-                    row=2, column=0, padx=10, pady=5)
-        
+            row=2, column=0, padx=10, pady=5)
+
         # Button 1 Config
-        buttonbox.configure(buttonbox, text='Yes', command=lambda: (quit(), destroy()))
-        
+        buttonbox.configure(buttonbox, text='Yes',
+                            command=lambda: (quit(), destroy()))
+
         # Button 2 Appear
         buttonbox2.pack(side=LEFT, padx=5)
 
@@ -82,13 +82,12 @@ def drop_box():
     buttonbox2 = Frame(root)
     buttonbox2.grid(row=4, column=0, columnspan=2, pady=5)
     buttonbox2 = Button(buttonbox2, text='No', command=root.destroy)
-    buttonbox2.pack_forget()#(side=LEFT, padx=5)
+    buttonbox2.pack_forget()  # (side=LEFT, padx=5)
 
- 
     #### Listbox to drag & drop files ###
 
     listbox = Listbox(root, name='drop_listbox',
-                        selectmode='extended', width=1, height=1)
+                      selectmode='extended', width=1, height=1)
     listbox.grid(row=1, column=0, padx=5, pady=5, sticky='news')
 
     ### Drop callbacks can be shared between the Listbox ###
@@ -120,13 +119,12 @@ def drop_box():
     #     mybutton = tk.Button(frame, text = "Ok", command = root.destroy)
     #     mybutton.pack(padx = 5, pady = 10)
 
-
     def drop(event):
         if event.data:
             # print('Dropped data:\n', event.data)
             # print_event_info(event)
             if event.widget == listbox:
-                
+
                 # Catch and Use File Name
                 global listbox_files
 
@@ -142,19 +140,19 @@ def drop_box():
                         print('Not dropping file "%s": file does not exist.' % f)
             else:
                 print('Error: reported event.widget not known')
-                
-        return event.action
-    
-    # Make the Listbox Drop Target
-    listbox.drop_target_register(DND_FILES)#, DND_TEXT)
 
-    for widget in (listbox,): #figure out why this needs space!!!
+        return event.action
+
+    # Make the Listbox Drop Target
+    listbox.drop_target_register(DND_FILES)  # , DND_TEXT)
+
+    for widget in (listbox,):  # figure out why this needs space!!!
         widget.dnd_bind('<<DropEnter>>', drop_enter)
         widget.dnd_bind('<<DropPosition>>', drop_position)
         widget.dnd_bind('<<DropLeave>>', drop_leave)
         widget.dnd_bind('<<Drop>>', drop)
-        #widget.dnd_bind('<<Drop:DND_Files>>', drop)
-        #widget.dnd_bind('<<Drop:DND_Text>>', drop)
+        # widget.dnd_bind('<<Drop:DND_Files>>', drop)
+        # widget.dnd_bind('<<Drop:DND_Text>>', drop)
 
     # Define Drag Callbacks
     def drag_init_listbox(event):
@@ -169,10 +167,10 @@ def drop_box():
         # action type and DnD type:
         return ((ASK, COPY), (DND_FILES, DND_TEXT), data)
 
-
     def drag_end(event):
         # print_event_info(event)
-        # this callback is not really necessary if it doesn't do anything useful
+
+        # this callback doesn't do anything useful
         print('Drag ended for widget:', event.widget)
 
         # Make the widget a drag source
@@ -181,7 +179,6 @@ def drop_box():
 
     listbox.dnd_bind('<<DragInitCmd>>', drag_init_listbox)
     listbox.dnd_bind('<<DragEndCmd>>', drag_end)
-
 
     root.update_idletasks()
 
@@ -192,9 +189,11 @@ def drop_box():
 #################### End Drop Box ####################
 
 ### Look For Steghide / Xcode / Macports ###
+
+
 def find_files(path: str, glob_pat: str, ignore_case: bool = False):
     rule = re.compile(fnmatch.translate(glob_pat), re.IGNORECASE) if ignore_case \
-            else re.compile(fnmatch.translate(glob_pat))
+        else re.compile(fnmatch.translate(glob_pat))
     return [n for n in glob.iglob(os.path.join(path, '**'), recursive=True) if rule.match(n)]
 
 
@@ -226,17 +225,6 @@ def find_files(path: str, glob_pat: str, ignore_case: bool = False):
 #################### End Install Required Files Windows ####################
 
 
-
-
-
-
-
-
-
-
-
-
-
 #################### Start "Start" Window ####################
 
 def start_box():
@@ -248,34 +236,35 @@ def start_box():
 
         # Label 1 Config
         drop_box()
-        mylabel.config(text = listbox_files, bg = "blue", fg = "yellow")
-        mylabel.pack(padx = 5, pady = 10)
+        mylabel.config(text=listbox_files, bg="blue", fg="yellow")
+        mylabel.pack(padx=5, pady=10)
 
         # Label 2 Config
-        mylabel2.config(bg = "blue", fg = "yellow")
-        mylabel2.pack(padx = 5, pady = 10)
-        
-        # Button 1 Config
-        mybutton.config(text = "Yes", command = lambda: (quit(), destroy()))
-        mybutton.pack(padx = 5, pady = 10)
-        
-        # Button 2 Config
-        mybutton2.config(text = "No", command = root.destroy)
-        mybutton2.pack(padx = 5, pady = 10)
+        mylabel2.config(bg="blue", fg="yellow")
+        mylabel2.pack(padx=5, pady=10)
 
+        # Button 1 Config
+        mybutton.config(text="Yes", command=lambda: (quit(), destroy()))
+        mybutton.pack(padx=5, pady=10)
+
+        # Button 2 Config
+        mybutton2.config(text="No", command=root.destroy)
+        mybutton2.pack(padx=5, pady=10)
 
     # Count Clicks For Yes Button
     root.counter = 0
+
     def clicks():
         root.counter += 1
         if root.counter == 3:
-            mylabel.config(text = "Are You Positive!!!", bg = "red")
+            mylabel.config(text="Are You Positive!!!", bg="red")
         elif root.counter == 10:
-            mylabel.config(text = "You Must Want This Virus!!", bg = "red")
+            mylabel.config(text="You Must Want This Virus!!", bg="red")
         elif root.counter == 20:
-            mylabel.config(text = "Only a couple more clicks :(", bg = "red")
+            mylabel.config(text="Only a couple more clicks :(", bg="red")
         elif root.counter == 24:
-            mylabel.config(text = "You made it to 24 clicks!!", bg = "blue", fg = "yellow")
+            mylabel.config(text="You made it to 24 clicks!!",
+                           bg="blue", fg="yellow")
             change_theme()
         elif root.counter >= 24:
             change_theme()
@@ -284,29 +273,27 @@ def start_box():
         # mylabel.config(text = f"{root.counter}", bg = "blue", fg = "yellow")
         # mylabel.pack(padx = 5, pady = 10)
 
-
     # Label 1
-    mylabel = tk.Label(frame, text = "This is a VIRUS!", bg = "red")
-    mylabel.pack(padx = 5, pady = 10)
+    mylabel = tk.Label(frame, text="This is a VIRUS!", bg="red")
+    mylabel.pack(padx=5, pady=10)
 
     # Label 2
-    mylabel2 = tk.Label(frame, text = "Do You Want to Continue?", bg = "red")
-    mylabel2.pack(padx = 5, pady = 10)
+    mylabel2 = tk.Label(frame, text="Do You Want to Continue?", bg="red")
+    mylabel2.pack(padx=5, pady=10)
 
     # Button 1
-    mybutton = tk.Button(frame, text = "Yes", command = clicks)
-    mybutton.pack(padx = 5, pady = 10)
+    mybutton = tk.Button(frame, text="Yes", command=clicks)
+    mybutton.pack(padx=5, pady=10)
 
     # Button 2
-    mybutton2 = tk.Button(frame, text = "No", command = root.destroy)
-    mybutton2.pack(padx = 5, pady = 10)
+    mybutton2 = tk.Button(frame, text="No", command=root.destroy)
+    mybutton2.pack(padx=5, pady=10)
 
-    frame.pack(padx = 5, pady = 5)
+    frame.pack(padx=5, pady=5)
 
     root.mainloop()
 
 #################### End "Start" Window ####################
 
+
 start_box()
-
-
